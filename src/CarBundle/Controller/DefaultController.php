7b4d48ad793958666a7a6b5aa88a98b9ade5512c
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-05-09 09:10:26 +0100

    adding car entity and car repository + update html views + new route in default car controller

--- src/CarBundle/Controller/DefaultController.php
+++ src/CarBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace CarBundle\Controller;
 
+use CarBundle\Entity\Car;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -12,14 +13,23 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        $cars = [
-            ['make' => 'BMW', 'name' => 'X1'],
-            ['make' => 'Fiat', 'name' => 'Croma'],
-            ['make' => 'Audi', 'name' => 'Q7'],
-        ];
+        $carRepository = $this->getDoctrine()->getRepository(Car::class);
+        $cars = $carRepository->findAll();
 
         //now we need to pass our array to a view - the second param in the render function below:
 
         return $this->render('CarBundle:Default:index.html.twig', ['cars' => $cars]);
+    }
+
+    /**
+     * @Route("/car/{id}", name="show_car")
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showAction($id)
+    {
+        $carRepository = $this->getDoctrine()->getRepository(Car::class);
+        $car = $carRepository->find($id);
+        return $this->render('CarBundle:Default:show.html.twig', ['car' => $car]);
     }
 }
