9460a7264c86de7488f2ccbbf154cb3036e48c0c
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-05-14 21:05:13 +0100

    adding search bar + form on the front end

--- src/CarBundle/Controller/DefaultController.php
+++ src/CarBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace CarBundle\Controller;
 use CarBundle\Entity\Car;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DefaultController extends Controller
 {
@@ -16,9 +17,19 @@ class DefaultController extends Controller
         $carRepository = $this->getDoctrine()->getRepository(Car::class);
         $cars = $carRepository->findCarsWithDetails();
 
-        //now we need to pass our array to a view - the second param in the render function below:
+        //creating a search form (i just watched the video here + then copied and pasted his code)
+        $form = $this->createFormBuilder()
+            ->setMethod('GET')
+            ->add('search', TextType::class)
+            ->getForm();
 
-        return $this->render('CarBundle:Default:index.html.twig', ['cars' => $cars]);
+        //now we need to pass our array (of cars) to a view - the second param in the render function below:
+        return $this->render('CarBundle:Default:index.html.twig',
+            [
+                'cars' => $cars,
+                'form' => $form->createView()
+            ]
+        );
     }
 
     /**
